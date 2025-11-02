b28c4739cedf6158b735e8c181492a4eef30b235
Sasha Stopochkin <stopochkin23@gmail.com>
2025-11-02 16:59:03 +0200

    implement py-distance-class

--- app/main.py
+++ app/main.py
@@ -1,3 +1,27 @@
 class Distance:
-    # Write your code here
-    pass
+    def __init__(self, km):
+        self.km = km
+    def __str__(self):
+        print(f"Distance: {self.km} kilometers")
+    def __repr__(self):
+        print(f"Distance(km={self.km})")
+    def __add__(self, other):
+        return Distance(self.km + other.km)
+    def __iadd__(self, other):
+        test = self + other
+        print(f"{self.km} is {test}")
+    def __mul__(self, other):
+        return Distance(self.km * other.km)
+    def __truediv__(self, other):
+        return Distance(self.km / other.km)
+    def __lt__(self, other):
+        return Distance(self.km < other.km)
+    def __gt__(self, other):
+        return Distance(self.km > other.km)
+    def __eq__(self, other):
+        return Distance(self.km == other.km)
+    def __le__(self, other):
+        return Distance(self.km <= other.km)
+    def __ge__(self, other):
+        return Distance(self.km >= other.km)
+
