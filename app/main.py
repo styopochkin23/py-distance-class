435e8ab599e4720d731252d54e8d7accc1b0b776
Sasha Stopochkin <stopochkin23@gmail.com>
2025-11-02 17:34:50 +0200

    implement3 py-distance-class

--- app/main.py
+++ app/main.py
@@ -1,27 +1,45 @@
 class Distance:
-    def __init__(self, km):
+    def __init__(self, km: float) -> None:
         self.km = km
-    def __str__(self):
+
+    def __str__(self) -> str:
         return f"Distance: {self.km} kilometers"
-    def __repr__(self):
+
+    def __repr__(self) -> str:
         return f"Distance(km={self.km})"
-    def __add__(self, other):
+
+    def __add__(self, other: "Distance | float | int") -> "Distance":
+        if isinstance(other, Distance):
+            return Distance(self.km + other)
         return Distance(self.km + other.km)
-    def __iadd__(self, other):
-        test = self + other
-        return f"{self.km} is {test}"
-    def __mul__(self, other):
+
+    def __iadd__(self, other: "Distance | float | int") -> "Distance":
+        if isinstance(other, Distance):
+            self.km += other
+        else:
+            self.km += other.km
+        return f"{self.km} is {self}"
+
+    def __mul__(self, other: int | float) -> "Distance":
         return self.km * other.km
-    def __truediv__(self, other):
-        return self.km / other.km
-    def __lt__(self, other):
+
+    def __truediv__(self, other: int | float) -> "Distance":
+        return Distance(round(self.km / other, 2))
+
+    def __lt__(self, other: "Distance") -> bool:
         return self.km < other.km
-    def __gt__(self, other):
+
+    def __gt__(self, other: "Distance") -> bool:
         return self.km > other.km
-    def __eq__(self, other):
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Distance):
+            return False
         return self.km == other.km
-    def __le__(self, other):
+
+    def __le__(self, other: "Distance") -> bool:
         return self.km <= other.km
-    def __ge__(self, other):
+
+    def __ge__(self, other: "Distance") -> bool:
         return self.km >= other.km
 
